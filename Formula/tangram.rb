1138d28f27f9f218656cfca7dd83dcf0bfb31863
Mike English <mike.english@atomicobject.com>
2013-03-16 11:28:03 -0400

    update tangram formula to v0.1-1

--- Formula/tangram.rb
+++ Formula/tangram.rb
@@ -2,16 +2,16 @@ require 'formula'
 
 class Tangram < Formula
   homepage 'https://github.com/silentbicycle/tangram'
-  url 'https://github.com/silentbicycle/tangram/archive/v0.1-0.tar.gz'
-  sha1 '74e0c4000982139eae3abe82ce7f3d51b24f1dc4'
-  version '0.1-0'
+  url 'https://github.com/silentbicycle/tangram/archive/v0.1-1.tar.gz'
+  sha1 '34bd7022d0faad96145cd219872ed9d46b3598bb'
+  version '0.1-1'
 
   depends_on 'lua'
   depends_on 'luarocks'
   depends_on 'hashchop'
 
   def install
-    system 'luarocks install tangram-0.1-0.rockspec'
+    system 'luarocks install tangram-0.1-1.rockspec'
   end
   def test
     system 'tangram test'
